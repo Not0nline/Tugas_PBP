fd95bdb2dd44e202ccee669288992f427ae7b91c
Not0nline <adrial2_nathanael@yahoo.com>
2023-09-07 01:29:14 +0700

    app sudah bisa jalan

--- main/views.py
+++ main/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
 def show_main(request):
+    data_list = [
+        {'name': 'Brain Cells', 'amount': 1, 'description': 'Very Important'},
+        {'name': 'Sanity', 'amount': 0, 'description': 'Very Important'},
+        {'name': 'Pressure', 'amount': 100, 'description': 'Make Stressed'},
+    ]
     context = {
-        'name': 'Pak Bepe',
-        'amount': 69,
-        'description': 'yes'
+        'data_list': data_list
     }
 
     return render(request, "main.html", context)
